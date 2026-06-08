3065299f4935078846074522d6d921acfc6208c5
Khôi Tran <kt@compile.ch>
2026-06-08 23:21:23 +0200

    kde

--- modules/kde.nix
+++ modules/kde.nix
@@ -1,11 +1,10 @@
-{ pkgs, ... }: {
+{ ... }: {
   services.desktopManager.plasma6.enable = true;
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     theme = "breeze";
-    package = pkgs.kdePackages.sddm;
   };
 
   # Audio
