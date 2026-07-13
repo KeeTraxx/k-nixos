65f4ead367598372f202aabef0fbdaf7c49f631d
Khôi Tran <kt@compile.ch>
2026-07-13 21:17:23 +0200

    patch desktop files correctly

--- home-manager-only/nixgl-wrap.nix
+++ home-manager-only/nixgl-wrap.nix
@@ -17,10 +17,24 @@
           makeWrapper "$nixgl_bin" $out/bin/$(basename $bin) \
             --add-flags "$bin"
         done
-        for dir in share lib etc; do
+        for dir in lib etc; do
           if [ -d "${pkg}/$dir" ]; then
             ln -s "${pkg}/$dir" "$out/$dir"
           fi
         done
+        if [ -d "${pkg}/share" ]; then
+          mkdir -p $out/share
+          for entry in ${pkg}/share/*; do
+            if [ "$(basename $entry)" = applications ]; then
+              # rewrite desktop entries to point at the wrapped binaries
+              mkdir $out/share/applications
+              for f in $entry/*; do
+                sed "s|${pkg}/bin|$out/bin|g" "$f" > "$out/share/applications/$(basename $f)"
+              done
+            else
+              ln -s "$entry" $out/share/
+            fi
+          done
+        fi
       '';
 }
